14332770b20869a58e26b99550262c2b13ff9629
nilesh9758 <153944540+nilesh9758@users.noreply.github.com>
2025-06-09 04:27:20 +0530

    Create PendingUser.js

---
+++ backend/models/PendingUser.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const pendingSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  code: String,
+  expires: Number,
+});
+
+module.exports = mongoose.models.PendingUser || mongoose.model('PendingUser', pendingSchema);
